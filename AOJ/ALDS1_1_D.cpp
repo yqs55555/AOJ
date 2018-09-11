c68b8f7609e017bfb1da15fb2ea1674c82492453
飞墨留白 <849561570@qq.com>
2018-09-11 17:26:08 +0800

    ALDS1_1_D

--- AOJ/ALDS1_1_D.cpp
+++ AOJ/ALDS1_1_D.cpp
@@ -1,9 +1,21 @@
 #if 1
+//date : 2018.9.11
 #include <iostream>
 using namespace std;
 int main()
 {
-
+	int n = 0, x;
+	int max_profit = -2e9, min;
+	cin >> n;
+	cin >> min;
+	n--;
+	while (n--)
+	{
+		cin >> x;
+		max_profit = x - min > max_profit ? x - min : max_profit;
+		min = x < min ? x : min;
+	}
+	cout << max_profit << endl;
 	return 0;
 }
 #endif
